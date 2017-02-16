3a683e60f16c31f8f965777d4be59f3d5435469e
Andrew Dalton and Micky Matthews <micky@>
2017-02-15 20:48:17 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var convTsp = function(teaspoon){
+  return teaspoon / 3;
+}
+var convTbsp = function(tablespoon){
+  return tablespoon / 16;
+}
+var convCup = function(cup){
+  return cup / 2;
+}
+var convPint = function(pint) {
+  return pint / 2;
+}
+var convQuart = function(quart) {
+  return quart / 4;
+};
+
+$(document).ready(function()  {
+  $("form#convTsp").submit(function(event) {
+    var number1 = parseInt($("#teaspoon").val());
+    var number2 = parseInt($("#tablespoon".val());
+    var result =
+  })
